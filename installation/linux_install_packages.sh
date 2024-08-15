b63dd252d1363b2de0098f72c1476048852b7050
Abdullah Khawer <abdullah.khawer@mendix.com>
2024-08-15 16:12:58 +0200

    fix: Add no cache options for package installers to reduce installation size.

--- installation/linux_install_packages.sh
+++ installation/linux_install_packages.sh
@@ -1,15 +1,12 @@
 #!/bin/bash
 set -ex
 
-# update package lists
-apk update
-
 # install Git, jq, Bash, Make, wget, Python3, pip, Go (Golang)
-apk add git jq bash make wget python3 py3-pip go
+apk add --no-cache git jq bash make wget python3 py3-pip go
 git --version && jq --version && bash --version && make --version && wget --version && python3 --version && pip --version && go version
 
 # install "Python Atlassian REST API Wrapper", "World timezone definitions, modern and historical" and "Requests" Python libraries
-pip install atlassian-python-api pytz requests --break-system-packages
+pip install --no-cache-dir atlassian-python-api pytz requests || pip install atlassian-python-api pytz requests --break-system-packages
 
 # install Gitleaks
 rm -rf /usr/local/gitleaks && git clone https://github.com/gitleaks/gitleaks.git /usr/local/gitleaks
