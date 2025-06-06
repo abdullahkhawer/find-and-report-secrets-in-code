a46f4d87528761b2547b11f37665f02555752572
Abdullah Khawer <abdullah.khawer@mendix.com>
2025-06-06 18:14:56 +0200

    feat: Upgrade Gitleaks, add the capability to tag commit authors in Slack notification message, refactor code, fix bugs, and update READMEs accordingly.

--- gitleaks.sh
+++ gitleaks.sh
@@ -1,43 +1,59 @@
 #!/bin/bash
-
+# shellcheck disable=SC2002
 echo "Script Execution Started!"
 
 # remove Gitleaks reports if they exist already
 echo "Removing Gitleaks reports if they exist already..."
-rm -rf ${LOCAL_PATH_TO_GIT_REPO}/gitleaks-report-detailed.json
+rm -rf "${LOCAL_PATH_TO_GIT_REPO}/gitleaks-report-detailed.json"
 rm -rf ./gitleaks-report.json
 
 # run Gitleaks to find secrets and generate a detailed report in JSON for the secrets found
 echo "Running Gitleaks to find secrets and generating a detailed report in JSON for the secrets found..."
-gitleaks detect -r ${LOCAL_PATH_TO_GIT_REPO}/gitleaks-report-detailed.json -f json -s ${LOCAL_PATH_TO_GIT_REPO} --redact --no-git
+echo -n "Gitleaks v" && gitleaks version
+gitleaks detect -r "${LOCAL_PATH_TO_GIT_REPO}/gitleaks-report-detailed.json" -f json -s "${LOCAL_PATH_TO_GIT_REPO}" --redact --no-git
 
 # create a final report in JSON using the detailed report having relevant information only
 echo "Creating a final report in JSON using the detailed report having relevant information only..."
-file_contents=$(cat ${LOCAL_PATH_TO_GIT_REPO}/gitleaks-report-detailed.json)
-if [ "$file_contents" != "[]" ]; then
+file_contents=$(cat "${LOCAL_PATH_TO_GIT_REPO}/gitleaks-report-detailed.json")
+if [ "${file_contents}" != "[]" ]; then
     echo "[" > ./gitleaks-report.json
-    cat ${LOCAL_PATH_TO_GIT_REPO}/gitleaks-report-detailed.json | jq -c '.[]' | while read -r line; do
-        description=$(jq -r '.Description' <<< "$line")
-        start_line=$(jq -r '.StartLine' <<< "$line")
-        file=$(jq -r '.File' <<< "$line")
-        file=$(echo "$file" | sed "s|^${LOCAL_PATH_TO_GIT_REPO}/||")
-        secret_type=$(jq -r '.RuleID' <<< "$line")
+    cat "${LOCAL_PATH_TO_GIT_REPO}/gitleaks-report-detailed.json" | jq -c '.[]' | while read -r line; do
+        description=$(jq -r '.Description' <<< "${line}")
+        start_line=$(jq -r '.StartLine' <<< "${line}")
+        file=$(jq -r '.File' <<< "${line}")
+        file="${file#"${LOCAL_PATH_TO_GIT_REPO}"/}"
+        secret_type=$(jq -r '.RuleID' <<< "${line}")
+        url="${REMOTE_PATH_TO_GIT_REPO}/-/blob/${BRANCH_NAME}/${file}"
+        url="${url// /%20}"
 
-        # use 'git blame' to find the commit id and author for each finding
-        blame=$(cd ${LOCAL_PATH_TO_GIT_REPO} && git blame -L "$start_line","$start_line" ./"$file" --porcelain)
-        commit_id=$(echo "$blame" | awk 'NR==1' | awk -F ' ' '{print $1}')
-        author=$(echo "$blame" | awk 'NR==2' | awk -F 'author ' '{print $2}')
+        if [[ "${start_line}" == "0" ]]; then
+            # use 'git log' to find the commit id, commit author and commit author's email for the finding
+            git_log=$(cd "${LOCAL_PATH_TO_GIT_REPO}" && git log -1 ./"${file}")
+            commit_id=$(echo "${git_log}" | awk 'NR==1' | awk -F ' ' '{print $2}')
+            commit_author=$(echo "${git_log}" | awk 'NR==2' | awk -F 'Author: ' '{print $2}' | awk -F ' <' '{print $1}')
+            commit_author_email=$(echo "${git_log}" | awk 'NR==2' | awk -F 'Author: ' '{print $2}' | awk -F ' <' '{print $2}')
+            commit_author_email="${commit_author_email%>}"
+        else
+            url="${url}#L${start_line}"
+            # use 'git blame' to find the commit id, commit author and commit author's email for the finding
+            git_blame=$(cd "${LOCAL_PATH_TO_GIT_REPO}" && git blame -L "${start_line}","${start_line}" ./"${file}" --porcelain)
+            commit_id=$(echo "${git_blame}" | awk 'NR==1' | awk -F ' ' '{print $1}')
+            commit_author=$(echo "${git_blame}" | awk 'NR==2' | awk -F 'author ' '{print $2}')
+            commit_author_email=$(echo "${git_blame}" | awk 'NR==3' | awk -F 'author-mail <' '{print $2}')
+            commit_author_email="${commit_author_email%>}"
+        fi
 
         # append final JSON objects to the new report
         jq -n \
-            --arg desc "$description" \
-            --arg file "$file" \
-            --arg line_no "$start_line" \
-            --arg url "${REMOTE_PATH_TO_GIT_REPO}/-/blob/${BRANCH_NAME}/${file}#L${start_line}" \
-            --arg type "$secret_type" \
-            --arg commit "$commit_id" \
-            --arg author "$author" \
-            '{"Description": $desc, "File": $file, "Line No.": $line_no, "Link": $url, "Secret Type": $type, "Commit": $commit, "Author": $author}' >> ./gitleaks-report.json
+            --arg desc "${description}" \
+            --arg file "${file}" \
+            --arg line_no "${start_line}" \
+            --arg url "${url}" \
+            --arg type "${secret_type}" \
+            --arg commit_id "${commit_id}" \
+            --arg commit_author "${commit_author}" \
+            --arg commit_author_email "${commit_author_email}" \
+            '{"Description": $desc, "File": $file, "Line No.": $line_no, "Link": $url, "Secret Type": $type, "Commit ID": $commit_id, "Commit Author": $commit_author, "Commit Author Email": $commit_author_email}' >> ./gitleaks-report.json
 
         echo "," >> ./gitleaks-report.json
     done
--- installation/macos_install_packages.sh
+++ installation/macos_install_packages.sh
@@ -5,17 +5,17 @@ set -ex
 brew install git jq bash python python@3
 git --version && jq --version && bash --version && pip --version && python3 --version
 
-# install "Python Atlassian REST API Wrapper", "World timezone definitions, modern and historical" and "Requests" Python libraries
-pip install --no-cache-dir atlassian-python-api pytz requests || pip install atlassian-python-api pytz requests --break-system-packages
+# install "Atlassian", "World timezone definitions", "Requests" and "Slack" Python libraries
+pip install --no-cache-dir atlassian-python-api pytz requests slack-sdk || pip install atlassian-python-api pytz requests slack-sdk --break-system-packages
 
 # install Gitleaks
 sudo rm -rf /usr/local/gitleaks && sudo git clone https://github.com/gitleaks/gitleaks.git /usr/local/gitleaks
 cd /usr/local/gitleaks
 git config --global --add safe.directory /usr/local/gitleaks
-sudo git checkout tags/v8.18.4
+sudo git checkout tags/v8.27.0
 sudo make build
 cd /
 export PATH=$PATH:/usr/local/gitleaks
-echo -n "gitleaks " && gitleaks version
+echo -n "Gitleaks v" && gitleaks version
 
 echo "Installation completed successfully."
